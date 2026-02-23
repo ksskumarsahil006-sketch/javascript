b5847e1caadf69e58fa69addc74ee505e5f9bfad
Kumar Sahil Sonal <ksskumarsahil006@gmail.com>
2026-02-23 19:02:56 +0000

    Add  chaiaurcode1.js

---
+++ chaiaurcode1.js
@@ -0,0 +1,13 @@
+console.log("Sahil")
+const accountId = 43243;
+let accountEmail = "kumar.240103050@iiitbh.ac.in";
+const rollNo = 240103050;
+var semester =1;
+console.log(rollNo)
+let name
+//accountId = 45;
+/*
+never use var cause it causes error in maintaining thriugh more codes
+*/
+console.log(accountId)
+console.table([accountId,rollNo,accountEmail,semester,name])
