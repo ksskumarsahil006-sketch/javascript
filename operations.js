24ca83ffa4c6d1ecf5ca86f5b38451dc364c0a52
Kumar Sahil Sonal <ksskumarsahil006@gmail.com>
2026-02-23 19:07:34 +0000

    Add operations.js

---
+++ operations.js
@@ -0,0 +1,15 @@
+let value = 3;
+let score = value+45;
+let str = score+"abc";//number ko string mein convert krke string addition perform ki gti hai 
+
+console.log(value,typeof(value));//3 nuymber 
+console.log(score,typeof score);//48 number
+console.log(str, typeof str);//48abc string
+
+//Normal opearations
+console.log(2+2);//Number addition 
+console.log(2-2);//NUmber substraction
+console.log(2*2);//Number multiplication
+console.log(2/2);//Number division
+console.log(2**2);//Number power 2 to the power 2 if(2**3) it means 2 to the power 3
+console.log(2%3);//Number modulo opearator
