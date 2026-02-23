0ec92c295d46c1d434e641971f85807bf2edae73
Kumar Sahil Sonal <ksskumarsahil006@gmail.com>
2026-02-23 19:10:50 +0000

    Add temo.js

---
+++ temo.js
@@ -0,0 +1,46 @@
+// // let ch = "Sahil";
+// // const cha =  "Sonal";
+// // let first_name = prompt("Enter your first name");
+// // console.log(first_name+ch+cha);
+// let n1 = 2;
+// console.log(n1)
+
+// let n2 = 1.3;
+// console.log(n2)
+
+// let n3 = Infinity;
+// console.log(n3)
+
+// let n4 = "something here too" ;
+// console.log(n4)
+// let pattern = /hello/;
+
+// Testing the pattern against a string
+// Returns true because "hello" is present
+// let result = pattern.test("hello, world!"); 
+
+// console.log(result);
+// result = pattern.test("Hello,World!");
+//Return false as hello is not present because Hello is there
+// console.log(result);
+//  DATE AND TIME
+// Creating a new Date object for the
+// current date and time
+let currentDate = new Date();
+
+// Displaying the current date and time
+console.log(currentDate);  
+let s1 = Symbol("abc");
+let s2 = Symbol("abc");
+console.log(s1 === s2);
+//Symbol kabhi bhi ek dusre se same nhi hoga\
+//var agar use krte hain kisi function mein to usko uske nested function mein bh use kr skte hain
+console.log(10 > 5);
+console.log(10 == "10");//ye true hoga 
+console.log(10==="10")//ye fasle hoga kyunki === ye type bhi compare krta hai
+// & performs a bitwise AND.
+// | performs a bitwise OR.
+// ^ performs a bitwise XOR.
+// ~ performs a bitwise NOT
+//TERNERY Operator
+//condition ? expression1 : expression2 evaluates expression1 if the condition is true, otherwise evaluates expression2.
