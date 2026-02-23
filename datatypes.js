f9dfc218711f52df510ba3df9224fab2882f09ac
Kumar Sahil Sonal <ksskumarsahil006@gmail.com>
2026-02-23 19:05:30 +0000

    Add datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+//treat all JS code as newer version
+//alert(3+3) ye tabhi kaam kregi jb console mein y html code se documentation se run hoga 
+console.log(3+3)
+console.log("datatypes discuss hoga ab")
+
+let name  = " Sahil"
+let age = 28
+//ECMA standards datatype padhna hai
+//numer ki rangle 2 to the power 53 hai
+//big integer bhi data type hai 
+// string bhi data type hai
+//boolean bhi present hai
+//NULL standlone value . ye ek object hai;
+//undefined ye t hoti hai jb variable declare kr di gyo ho bt vaue asssign nhi ki gyi ho
+//symbol unique element
+//object
+console.log(typeof null)
