184b753ba2ebf08a35393c57a59120e0b75d0979
Kumar Sahil Sonal <ksskumarsahil006@gmail.com>
2026-02-23 19:09:21 +0000

    Add conversionofdatatypes.js

---
+++ conversionofdatatypes.js
@@ -0,0 +1,27 @@
+//let score = '14';
+//console.log(typeof score);
+//let valueinnumber = Number (score);
+//console.log(valueinnumber);
+//agr score mein abc hota like 
+/*
+    let score = "14abcd";
+    let val = Number(score);
+    console.log(typeof(value));
+    console.log(value);
+    is code mein value to number assign ho jayegi it means
+    data type conversion ho jayega bt koi numerical value assign nhi hogi
+    agr hm score ko null assign kr de phir 0 ho jaayega;
+    true boolean pe 1 aa jayega
+    "33abc"=>Nan;
+    "33"=>33;
+    true=>1;
+    false=>0;
+
+*/
+let score = "adnsn";
+let booleanvalue = Boolean(score);
+console.log(booleanvalue);//true cause it has some value
+
+let value = "";
+let boolvalue = Boolean(value);
+console.log(boolvalue);//false cause it's empty
