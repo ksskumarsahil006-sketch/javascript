d0116f13b8c739954fcc0b164b4cf911b4438a29
Kumar Sahil Sonal <ksskumarsahil006@gmail.com>
2026-02-24 17:56:54 +0000

    Add Scope.js

---
+++ Scope.js
@@ -0,0 +1,15 @@
+//scope mtlb {} 
+
+
+
+if(true){
+   let a = 10;
+const b = 89;
+var c = 67;
+
+}
+console.log(c);
+//console.log(a); ye kaam nhi krega kyuki similarly 
+//console.log(b) bhi reference error dega 
+// but for console.log(c) value print kr dega 
+//kyunki var out of the scope access kr skte hain
